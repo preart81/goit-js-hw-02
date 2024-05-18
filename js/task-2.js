f05735372e19a0290037be271e7e6559f9524f4e
Artur Prepelytsia <preart81@gmail.com>
2024-05-18 15:37:24 +0300

    do task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,24 @@
 "use strict";
+function formatMessage(message, maxLength) {
+  // приймає рядок (параметр message) та перевіряє його довжину відповідно до
+  // заданої максимальної довжини (параметр maxLength).
+
+  // 🔸Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає
+  // початковий рядок без змін.
+
+  // 🔸Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength
+  // символів, додає трикрапку "..." в кінці та повертає обрізану версію.
+
+  if (message.length <= maxLength) {
+    return message;
+  } else {
+    return message.slice(0, maxLength) + "...";
+  }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
